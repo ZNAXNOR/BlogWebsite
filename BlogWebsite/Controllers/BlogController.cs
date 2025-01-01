762d7f2cab8f7559c0c92b952d442f064f67b78f
ZNAXNOR <omkarsdalvi1*@gmail.com>
2025-01-01 12:44:00 +0530

    Add project files.

---
+++ BlogWebsite/Controllers/BlogController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebsite.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // CRUD
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        // CRUD
+    }
+}
